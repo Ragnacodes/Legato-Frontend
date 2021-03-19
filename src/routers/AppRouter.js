dacf0969f1b38a0103b9368cb122fd7f38914fa8
Ysmn <lotfolahi.y@gmail.com>
2021-03-19 22:40:12 +0330

    router updated

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,6 +4,7 @@ import { createBrowserHistory } from 'history'
 import DashboardPage from '../components/DashboardPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
+import LandingPage from '../components/LandingPage/LandingPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
 
@@ -13,7 +14,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <PublicRoute path="/" component={LoginPage} exact={true} />
+        <PublicRoute path="/" component={LandingPage} exact={true} />
         <PrivateRoute path="/dashboard" component={DashboardPage} />
         <Route component={NotFoundPage} />
       </Switch>
