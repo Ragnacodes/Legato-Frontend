d4612498b5951bff26271924d883314dbf8f936a
Ysmn <lotfolahi.y@gmail.com>
2021-03-23 18:25:32 +0330

    login notifications

--- src/components/LoginSignUp/LoginForm.js
+++ src/components/LoginSignUp/LoginForm.js
@@ -1,102 +1,94 @@
-import React from 'react';
-import {useState} from 'react';
-import {TextField, Button} from '@material-ui/core'
-import '../../styles/login-signup.scss'
+import React from "react";
+import { connect } from "react-redux";
+import axios from "axios";
 
-import {connect} from 'react-redux';
-import * as actions from '../../actions/login';
-import {login} from '../../actions/auth';
-import axios from 'axios';
-function LoginForm({info, updateInfo, saveToken}) {
+import * as actions from "../../actions/login";
+import { login } from "../../actions/auth";
 
-    const sendData = () =>
-    {
-        axios.post('http://localhost:8080/api/auth/login', {
-            ...info,
-          })
-          .then(function (response) {
-            console.log(response);
-            saveToken(response.data.access_token)
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+import { TextField, Button } from "@material-ui/core";
+import "../../styles/login-signup.scss";
 
-        // fetch('http://localhost:8080/api/auth/login', {
-        // method: 'POST',
-        // body: JSON.stringify({
-        //     ...info,
-        // })
-        // })
-        // .then((response) =>
-        // {
-        //     console.log("response",response.json())
-        //     // saveToken(response.data.access_token)
-        // })
-        // .then(data =>{
-        //     console.log("data",data)
-        // });
-    }
+import { successNotification, errorNotification } from "../Notification";
 
-    const onChange = e =>
-    {
-        updateInfo(e.target.name,e.target.value.trim())  
-    }
-    const onSubmit = e => {
-        
-        e.preventDefault();
-        console.log(info)
-        sendData()
+function LoginForm({ info, updateInfo, saveToken, closeDialog }) {
+  const sendData = () => {
+    axios
+      .post("http://localhost:8080/api/auth/login", {
+        ...info,
+      })
+      .then((response) => {
+        console.log(response);
+        successNotification("You are logged in!");
+        saveToken(response.data.access_token);
+        closeDialog();
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.response) {
+          errorNotification(error.response.data.message);
+        } else {
+          errorNotification(error.message);
+        }
+      });
+  };
 
-    };
-
+  const onChange = (e) => {
+    updateInfo(e.target.name, e.target.value.trim());
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(info);
+    sendData();
+  };
 
   return (
     <div className="App">
-     <form noValidate autoComplete="off" onSubmit={e => onSubmit(e)}
-     className="login-form">
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => onSubmit(e)}
+        className="login-form"
+      >
+        <TextField
+          className="text-field"
+          onChange={onChange}
+          name="username"
+          label="Username"
+          variant="outlined"
+          size="small"
+        />
 
-     <TextField
-        className="text-field"
-        onChange={onChange}
-        name="username"
-        label="Username"
-        variant="outlined"
-        size="small"
-    />
+        <TextField
+          type="password"
+          className="text-field"
+          onChange={onChange}
+          name="password"
+          label="Password"
+          variant="outlined"
+          size="small"
+        />
 
-    <TextField
-        type="password"
-        className="text-field"
-        onChange={onChange}
-        name="password"
-        label="Password"
-        variant="outlined"
-        size="small"
-    />
-
-    <Button
-        type='submit'
-        fullWidth
-        variant='contained'
-        color="primary"
-        className="form-button"
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className="form-button"
         >
-        Sign In
-    </Button>
-     </form>
+          Sign In
+        </Button>
+      </form>
     </div>
   );
 }
 
 const mapStateToProps = (state) => ({
-    info : state.login.login_info,
+  info: state.login.login_info,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    updateInfo: (type,data) => dispatch(actions.updateLoginInfo(type,data)),
-    saveToken: (token) => dispatch(login(token)),
+  updateInfo: (type, data) => dispatch(actions.updateLoginInfo(type, data)),
+  saveToken: (token) => dispatch(login(token)),
 });
-  
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
 
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
