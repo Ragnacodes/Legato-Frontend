cade197fa4677be45f0e4f47e6f6a7d8803fe0ce
Ysmn <lotfolahi.y@gmail.com>
2021-03-23 17:46:07 +0330

    signup component update

--- src/components/LoginSignUp/SignUpForm.js
+++ src/components/LoginSignUp/SignUpForm.js
@@ -1,231 +1,218 @@
-import React from 'react';
-import {useState,useEffect,useRef} from 'react';
-import {TextField, Button} from '@material-ui/core'
-import '../../styles/login-signup.scss'
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import * as actions from "../../actions/signup";
 
-import IconButton from '@material-ui/core/IconButton';
+import {
+  TextField,
+  Button,
+  Backdrop,
+  CircularProgress,
+  InputAdornment,
+  IconButton,
+} from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
-import InputAdornment from '@material-ui/core/InputAdornment';
+import { successNotification, errorNotification } from "../Notification";
+import "../../styles/login-signup.scss";
 
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import ErrorIcon from '@material-ui/icons/Error';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Backdrop from '@material-ui/core/Backdrop';
-import {connect} from 'react-redux';
-import * as actions from '../../actions/signup';
+function SignUpForm({
+  info,
+  errors,
+  updateInfo,
+  validateInfo,
+  closeDialog,
+  resetForm,
+}) {
+  const signupForm = useRef(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [send, setSend] = useState(false);
 
-function SignUpForm({info, errors,valid,
-    updateInfo, validateInfo}) {
-
-    const [showPassword, setShowPassword] = useState(false);
-    const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
-    const [loading, setLoading] = useState(false)
-    const [send, setSend] = useState(false)
-
-    const noError = () =>
-    {
-        for (const [key, value] of Object.entries(errors)) {
-          if(value)
-              return false
-        }
-        return true
+  const noError = () => {
+    for (const [key, value] of Object.entries(errors)) {
+      if (value) return false;
     }
+    return true;
+  };
 
-    useEffect(() => {
-      if(send)
-      {
-        if(noError())
-        {
-          sendData()
-          console.log("send")
-        }
+  useEffect(() => {
+    if (send) {
+      if (noError()) {
+        sendData();
+        console.log("send");
+      } else {
+        errorNotification("PLease enter valid data.");
       }
-      return () => {
-        setSend(false)
-      }
-    }, [errors])
-
-
-    const sendData = () =>
-    {
-        setLoading(true)
-        
-        fetch('http://localhost:8080/api/auth/signup', {
-          method: 'POST',
-          body: JSON.stringify({
-            ...info,
-            confirm:"",
-          })
-        })
-        .then((response) =>
-        {
-          setLoading(false)
-          console.log("response",response)
-        })
-        .catch((error)=>
-        {
-          setLoading(false)
-          console.log("error", error)
-        })
     }
-
-
-    const isEmpty = (object) =>
-    {
-        return Object.keys(object).length === 0 && object.constructor === Object
-    }
-
-
-
-    const validateAll = () =>
-    {
-        validateInfo("email",info["email"]);
-        validateInfo("password",info["password"]);
-        validateInfo("confirm",info["confirm"]);
-        validateInfo("username",info["username"]);
-    }
-
-    const onChange = e =>
-    {
-        updateInfo(e.target.name,e.target.value.trim())
-        validateInfo(e.target.name, e.target.value);
-    }
-
-    const onSubmit = e => {
-        
-        e.preventDefault();
-        setSend(true)
-        validateAll()
-        //delay
-        // if(noError())
-        // {
-        //   setSend(true)
-        //   // sendData()
-        // }
-        // else
-        // {
-        //   console.log(errors)
-        // }
+    return () => {
+      setSend(false);
     };
+  }, [errors]);
 
-    
-    
-    
+  const sendData = () => {
+    setLoading(true);
+    axios
+      .post("http://localhost:8080/api/auth/signup", {
+        ...info,
+        confirm: "",
+      })
+      .then((response) => {
+        setLoading(false);
+        closeDialog();
+        console.log("response", response);
+        signupForm.current.reset();
+        resetForm();
+        successNotification("You have created your account. Please log in.");
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response) {
+          errorNotification(error.response.data.message);
+        } else {
+          errorNotification(error.message);
+        }
+        console.log("error", error.message);
+      });
+  };
+
+  const validateAll = () => {
+    validateInfo("email", info["email"]);
+    validateInfo("password", info["password"]);
+    validateInfo("confirm", info["confirm"]);
+    validateInfo("username", info["username"]);
+  };
+
+  const onChange = (e) => {
+    updateInfo(e.target.name, e.target.value.trim());
+    validateInfo(e.target.name, e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setSend(true);
+    validateAll();
+  };
+
   return (
     <div>
       <Backdrop className="signup-backdrop" open={loading}>
-        {loading &&<CircularProgress color="secondary" />}
+        {loading && <CircularProgress color="secondary" />}
       </Backdrop>
-     <form noValidate autoComplete="off" onSubmit={e => onSubmit(e)}
-     className="signup-form">
 
-     <TextField
-        className="text-field"
-        required
-        onChange={onChange}
-        name="email"
-        error={!!errors["email"]}
-        label="Email"
-        helperText={errors["email"]}
-        variant="outlined"
-        size='small'
-    />
+      <form
+        ref={signupForm}
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => onSubmit(e)}
+        className="signup-form"
+      >
+        <TextField
+          className="text-field"
+          required
+          onChange={onChange}
+          name="username"
+          error={!!errors["username"]}
+          label="Username"
+          helperText={errors["username"]}
+          variant="outlined"
+          size="small"
+        />
 
-    <TextField
-    type={showPassword ? 'text' : 'password'}
-    autoComplete="false"
-    className="text-field"
-    required
-    onChange={onChange}
-    name="password"
-    error={!!errors["password"]}
-    label="Password"
-    helperText={errors["password"]}
-    variant="outlined"
-    InputProps={{
-        endAdornment:(
-        <InputAdornment position="end">
-          <IconButton
-            aria-label="toggle password visibility"
-            onClick={()=>setShowPassword((prev)=>!prev)}
-            onMouseDown={(e)=>e.preventDefault()}
-            edge="end"
-          >
-            {showPassword ? <Visibility /> : <VisibilityOff />}
-          </IconButton>
-        </InputAdornment>
-  )}}
-  size='small'
-    />
+        <TextField
+          className="text-field"
+          required
+          onChange={onChange}
+          name="email"
+          error={!!errors["email"]}
+          label="Email"
+          helperText={errors["email"]}
+          variant="outlined"
+          size="small"
+        />
 
-    <TextField
-    type={showPasswordConfirm ? 'text' : 'password'}
-    className="text-field"
-    required
-    onChange={onChange}
-    name="confirm"
-    error={!!errors["confirm"]}
-    label="Confirm Password"
-    helperText={errors["confirm"]}
-    variant="outlined"
-    InputProps={{
-        endAdornment:(
-        <InputAdornment position="end">
-          <IconButton
-            aria-label="toggle password visibility"
-            onClick={()=>setShowPasswordConfirm((prev)=>!prev)}
-            onMouseDown={(e)=>e.preventDefault()}
-            edge="end"
-          >
-            {showPasswordConfirm ? <Visibility /> : <VisibilityOff />}
-          </IconButton>
-        </InputAdornment>
-  )
-    }}
-    size='small'
-    />
+        <TextField
+          type={showPassword ? "text" : "password"}
+          autoComplete="false"
+          className="text-field"
+          required
+          onChange={onChange}
+          name="password"
+          error={!!errors["password"]}
+          label="Password"
+          helperText={errors["password"]}
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  onMouseDown={(e) => e.preventDefault()}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          size="small"
+        />
 
-<TextField
-        className="text-field"
-        required
-        onChange={onChange}
-        name="username"
-        error={!!errors["username"]}
-        label="Username"
-        helperText={errors["username"]}
-        variant="outlined"
-        size='small'
-    />
+        <TextField
+          type={showPasswordConfirm ? "text" : "password"}
+          className="text-field"
+          required
+          onChange={onChange}
+          name="confirm"
+          error={!!errors["confirm"]}
+          label="Confirm Password"
+          helperText={errors["confirm"]}
+          variant="outlined"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowPasswordConfirm((prev) => !prev)}
+                  onMouseDown={(e) => e.preventDefault()}
+                  edge="end"
+                >
+                  {showPasswordConfirm ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          size="small"
+        />
 
-    <Button
-    disabled={loading}
-        type='submit'
-        fullWidth
-        variant='contained'
-        className="form-button"
-        color="primary"
+        <Button
+          disabled={loading}
+          type="submit"
+          fullWidth
+          variant="contained"
+          className="form-button"
+          color="primary"
         >
-        Sign Up
-    </Button>
-     </form>
+          Sign Up
+        </Button>
+      </form>
     </div>
   );
 }
 
 const mapStateToProps = (state) => ({
-    info : state.signup.signup_info,
-    errors : state.signup.signup_errors,
+  info: state.signup.signup_info,
+  errors: state.signup.signup_errors,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    updateInfo: (type,data) => dispatch(actions.updateInfo(type,data)),
-    setError: (type,data) => dispatch(actions.setError(type,data)),
-    deleteError: (type) => dispatch(actions.deleteError(type)),
-    validateConfirmPassword: () => dispatch(actions.validateConfirmPassword()),
-    validateInfo: (type,data) => dispatch(actions.validateInfo(type,data)),
+  updateInfo: (type, data) => dispatch(actions.updateInfo(type, data)),
+  validateInfo: (type, data) => dispatch(actions.validateInfo(type, data)),
+  resetForm: () => dispatch(actions.resetSignupForm()),
 });
-  
-export default connect(mapStateToProps, mapDispatchToProps)(SignUpForm);
-  
 
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpForm);
