935dffed277c63abdfd16563c6587cb57aca81c8
Ysmn <lotfolahi.y@gmail.com>
2021-03-25 18:26:48 +0330

    reducers tests

---
+++ src/tests/reducers/login.test.js
@@ -0,0 +1,36 @@
+import loginReducer from "../../reducers/login";
+import { ActionTypes } from "../../actions/login";
+
+test("should update username for login", () => {
+  const data = "user";
+  const action = {
+    type: ActionTypes.UPDATE_LOGIN_INFO,
+    payload: {
+      type: "username",
+      data,
+    },
+  };
+  const state = loginReducer({}, action);
+  expect(state.login_info).toHaveProperty("username", data);
+});
+
+test("should update password for login", () => {
+  const data = "11111111";
+  const action = {
+    type: ActionTypes.UPDATE_LOGIN_INFO,
+    payload: {
+      type: "password",
+      data,
+    },
+  };
+  const state = loginReducer({}, action);
+  expect(state.login_info).toHaveProperty("password", data);
+});
+
+test("should reset form for login", () => {
+  const action = {
+    type: ActionTypes.RESET_LOGIN_FORM,
+  };
+  const state = loginReducer({}, action);
+  expect(state).toHaveProperty("login_info", {});
+});
