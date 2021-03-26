43bd6f85c5ad204b6d4087748a61d1a1fcf55036
Ysmn <lotfolahi.y@gmail.com>
2021-03-26 15:00:43 +0330

    landing page route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history'
-import DashboardPage from '../components/DashboardPage';
-import NotFoundPage from '../components/NotFoundPage';
-import LoginPage from '../components/LoginPage';
-import LandingPage from '../components/LandingPage/LandingPage';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import DashboardPage from "../components/DashboardPage";
+import NotFoundPage from "../components/NotFoundPage";
+import LoginPage from "../components/LoginPage";
+import LandingPage from "../components/LandingPage/LandingPage";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const history = createBrowserHistory();
 
@@ -14,7 +14,7 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <PublicRoute path="/" component={LandingPage} exact={true} />
+        <Route path="/" component={LandingPage} exact={true} />
         <PrivateRoute path="/dashboard" component={DashboardPage} />
         <Route component={NotFoundPage} />
       </Switch>
