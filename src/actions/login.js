5f80cd6a3384819f64b6daf89e42804d8f94e485
Ysmn <lotfolahi.y@gmail.com>
2021-03-26 14:33:02 +0330

    login error action

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,19 +1,34 @@
 export const ActionTypes = {
-    UPDATE_LOGIN_INFO : "UPDATE_LOGIN_INFO",
-    RESET_LOGIN_FORM: "RESET_LOGIN_FORM",
-}
+  UPDATE_LOGIN_INFO: "UPDATE_LOGIN_INFO",
+  RESET_LOGIN_FORM: "RESET_LOGIN_FORM",
+  SET_LOGIN_ERROR: "SET_LOGIN_ERROR",
+};
 
-
-export const updateLoginInfo = (type, data) => 
-({
-    type: ActionTypes.UPDATE_LOGIN_INFO,
-    payload: {type, data},
+export const updateLoginInfo = (type, data) => ({
+  type: ActionTypes.UPDATE_LOGIN_INFO,
+  payload: { type, data },
 });
 
-export const resetLoginForm = () => 
-({
-    type: ActionTypes.RESET_LOGIN_FORM,
+export const validateLoginInfo = (type, data) => {
+  return (dispatch) => {
+    if (data) {
+      dispatch(setLoginError(type, ""));
+    } else {
+      dispatch(setLoginError(type, "Required."));
+    }
+  };
+};
+
+export const setLoginError = (type, err) => {
+  return {
+    type: ActionTypes.SET_LOGIN_ERROR,
+    payload: {
+      type,
+      err,
+    },
+  };
+};
+
+export const resetLoginForm = () => ({
+  type: ActionTypes.RESET_LOGIN_FORM,
 });
-
-
-
--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,29 +1,39 @@
-import {ActionTypes} from "../actions/login";
+import { ActionTypes } from "../actions/login";
 
 const initialState = {
-    login_info : {},
-}
+  login_info: {},
+  login_errors: {},
+};
 const signupReducer = (state = initialState, action) => {
-    switch (action.type) {
-      
-      case ActionTypes.RESET_LOGIN_FORM:
-        return {
-          ...state,
-          login_info : {}
-        };
+  switch (action.type) {
+    case ActionTypes.RESET_LOGIN_FORM:
+      return {
+        ...state,
+        login_info: {},
+        login_errors: {},
+      };
 
-      case ActionTypes.UPDATE_LOGIN_INFO:
-        return {
-          ...state,
-          login_info : {
-            ...state.login_info,
-            [action.payload.type] : action.payload.data,
-          }
-        };
-      
-      default:
-        return state;
-    }
-  };
-  
-  export default signupReducer;
+    case ActionTypes.UPDATE_LOGIN_INFO:
+      return {
+        ...state,
+        login_info: {
+          ...state.login_info,
+          [action.payload.type]: action.payload.data,
+        },
+      };
+
+    case ActionTypes.SET_LOGIN_ERROR:
+      return {
+        ...state,
+        login_errors: {
+          ...state.login_errors,
+          [action.payload.type]: action.payload.err,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default signupReducer;
