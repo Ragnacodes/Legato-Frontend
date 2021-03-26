00285b39ea50e6fbd493ce4038c7af5d3624fefc
ali <ali_sedaghi@ymail.com>
2021-03-27 04:18:39 +0430

    [refactor] Replace node names with icons

--- src/components/Sketchpad/SketchpadSidebar.js
+++ src/components/Sketchpad/SketchpadSidebar.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import Divider from '@material-ui/core/Divider';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import CodeIcon from '@material-ui/icons/Code';
+import QueueMusicIcon from '@material-ui/icons/QueueMusic';
+import LanguageIcon from '@material-ui/icons/Language';
+import AddIcon from '@material-ui/icons/Add';
 
 export default () => {
   const onDragStart = (event, nodeType) => {
@@ -8,21 +15,30 @@ export default () => {
 
   return (
     <aside>
-      <div className="description">You can drag these nodes to the pane on the right.</div>
+      <div className="description">Services</div>
       <div className="dndnode" onDragStart={(event) => onDragStart(event, 'WebhookNode')} draggable>
-        Webhook
+        <LanguageIcon fontSize="large" className="icon webhook" />
       </div>
       <div className="dndnode" onDragStart={(event) => onDragStart(event, 'SpotifyNode')} draggable>
-        Spotify
+        <QueueMusicIcon fontSize="large" className="icon spotify" />
       </div>
       <div className="dndnode" onDragStart={(event) => onDragStart(event, 'TelegramNode')} draggable>
-        Telegram bot
+        <TelegramIcon fontSize="large" className="icon telegram" />
       </div>
       <div className="dndnode" onDragStart={(event) => onDragStart(event, 'SSHNode')} draggable>
-        SSH
+        <CodeIcon fontSize="large" className="icon ssh" />
       </div>
       <div className="dndnode" onDragStart={(event) => onDragStart(event, 'GithubNode')} draggable>
-        Github
+        <GitHubIcon fontSize="large" className="icon github" />
+      </div>
+
+      <Divider />
+      <div className="description">Flows</div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'add')} draggable>
+        <AddIcon fontSize="large" className="icon" />
+      </div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'add')} draggable>
+        <AddIcon fontSize="large" className="icon" />
       </div>
     </aside>
   );
