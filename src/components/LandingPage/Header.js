bb4bb4ba3fb2ecb80b4b868442072c3ce529d6e3
ali <ali_sedaghi@ymail.com>
2021-03-27 21:13:03 +0430

    [refactor] Remove unreachable check condition in header

--- src/components/LandingPage/Header.js
+++ src/components/LandingPage/Header.js
@@ -1,26 +1,21 @@
 import React from "react";
 import { useState } from "react";
-import { connect } from "react-redux";
 
 import LoginForm from "../LoginSignUp/LoginForm";
 import SignUpForm from "../LoginSignUp/SignUpForm";
 
-import MenuIcon from "@material-ui/icons/Menu";
 import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
 import {
   AppBar,
-  IconButton,
   Toolbar,
   Button,
   Typography,
-  Tooltip,
   Dialog,
   DialogContent,
   DialogContentText,
   DialogTitle,
 } from "@material-ui/core";
-import { Link } from "react-router-dom";
-export const Header = ({ isAuthenticated }) => {
+const Header = () => {
   const [signupOpen, setSignupOpen] = useState(false);
   const [loginOpen, setLoginOpen] = useState(false);
 
@@ -73,33 +68,23 @@ export const Header = ({ isAuthenticated }) => {
             <Button color="secondary" className="about-button">
               About Us
             </Button>
-            {isAuthenticated ? (
-              <Tooltip title="Dashboard">
-                <Link to="/dashboard">
-                  <IconButton color="secondary">
-                    <MenuIcon className="dashboard-icon" />
-                  </IconButton>
-                </Link>
-              </Tooltip>
-            ) : (
-              <div>
-                <Button
-                  color="secondary"
-                  onClick={() => setLoginOpen(true)}
-                  className="login-button"
-                >
-                  Sign In
-                </Button>
-                <Button
-                  onClick={() => setSignupOpen(true)}
-                  variant="contained"
-                  color="secondary"
-                  className="signup-button"
-                >
-                  Sign Up
-                </Button>
-              </div>
-            )}
+            <div>
+              <Button
+                color="secondary"
+                onClick={() => setLoginOpen(true)}
+                className="login-button"
+              >
+                Sign In
+              </Button>
+              <Button
+                onClick={() => setSignupOpen(true)}
+                variant="contained"
+                color="secondary"
+                className="signup-button"
+              >
+                Sign Up
+              </Button>
+            </div>
           </div>
         </Toolbar>
       </AppBar>
@@ -107,8 +92,4 @@ export const Header = ({ isAuthenticated }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  isAuthenticated: !!state.auth.uid,
-});
-
-export default connect(mapStateToProps)(Header);
+export default Header;
