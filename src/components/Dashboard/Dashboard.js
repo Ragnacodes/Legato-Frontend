8545be5f86a7752f312216cd05aaba26d0767ad3
arman <armanalaki@gmail.com>
2021-04-05 22:03:27 +0430

    [feat] Add chart to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import React, {useState} from 'react';
+import Paper from '@material-ui/core/Paper';
+import {
+  ArgumentAxis,
+  ValueAxis,
+  Chart,
+  SplineSeries,
+  Legend,
+  AreaSeries,
+} from '@devexpress/dx-react-chart-material-ui';
+import { Animation } from '@devexpress/dx-react-chart';
+import { easeBounceOut } from 'd3-ease';
 
-const Dashboard = () => (
-  <Container maxWidth="lg">
-    <Grid container>
-      <div>Dashboard Page!</div>
-    </Grid>
-  </Container>
-);
+const UsedChart = () => {
+  const [data, setData] = useState([
+    { dataTransferArgument: 10, dataTransferValue: 10 },
+    { dataTransferArgument: 22, dataTransferValue: 40 },
+    { dataTransferArgument: 28, dataTransferValue: 21 },
+    { operationArgument: 10, operationValue: 20 },
+    { operationArgument: 22, operationValue: 15 },
+    { operationArgument: 28, operationValue: 36 },
+  ]);
+  return (
+    <Paper elevation={15} style={{ margin: 10, marginLeft: 30, marginRight: 30 }}>
+      <Chart data={data} height={300} style={{ padding: 10 }}>
+        <ArgumentAxis />
+        <ValueAxis />
+        <Legend />
+        <SplineSeries valueField="operationValue" argumentField="operationArgument" name="Operations" />
+        <SplineSeries valueField="dataTransferValue" argumentField="dataTransferArgument" name="Data transfer" />
+        <Animation duration={2500} easing={easeBounceOut} />
+      </Chart>
+    </Paper>
+  )
+};
 
-export default Dashboard;
+export default UsedChart;
