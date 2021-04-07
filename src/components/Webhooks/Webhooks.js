8270460ee5a727a9958bec9197f060e168d5379e
Ysmn <lotfolahi.y@gmail.com>
2021-04-07 16:48:14 +0330

    [feat] add webhook button for appbar test

--- src/components/Webhooks/Webhooks.js
+++ src/components/Webhooks/Webhooks.js
@@ -1,26 +1,41 @@
 import React from "react";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import Container from "@material-ui/core/Container";
 import List from "@material-ui/core/List";
 import Webhook from "./Webhook";
+import { updateAppBar } from "../../actions/appbar";
+import { Button } from "@material-ui/core";
 
-const Webhooks = ({ webhooks }) => (
-  <div className="content-container">
-    <Container maxWidth="lg" className="webhooks-list">
-      <List>
-        {webhooks.map((w) => {
-          console.log(w);
-          return <Webhook key={w.id} webhook={w} />;
-        })}
-      </List>
-    </Container>
-  </div>
-);
+const Webhooks = ({ webhooks, updateAppBar }) => {
+  useEffect(() => {
+    updateAppBar(
+      "right_children",
+      <Button variant="contained" color="secondary">
+        Add Webhook
+      </Button>
+    );
+  }, []);
+  return (
+    <div className="content-container">
+      <Container maxWidth="lg" className="webhooks-list">
+        <List>
+          {webhooks.map((w) => {
+            console.log(w);
+            return <Webhook key={w.id} webhook={w} />;
+          })}
+        </List>
+      </Container>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   webhooks: state.webhooks.webhooks,
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  updateAppBar: (type, data) => dispatch(updateAppBar(type, data)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Webhooks);
