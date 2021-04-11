f04182879c18fbdfdb05fe96507536cb28dc158d
ali <ali_sedaghi@ymail.com>
2021-04-12 03:01:29 +0430

    [feat] Add start actions for scenarios to connect to server

--- src/actions/scenarios.js
+++ src/actions/scenarios.js
@@ -1,41 +1,43 @@
-import { v4 as uuid } from 'uuid';
-import axios from 'axios';
+import Axios from '../utils/axiosConfig';
 
-export const loadScenarios = async () => {
-    axios.get("http://localhost:8080/api/users/arman/scenarios",
-     {headers: {"Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFybWFuIiwiZXhwIjoxNjE3OTExODg4fQ.V7BR--E0tR2j-qIwlyBhDW2wOFG4K89nh6_O6HmGTIc"}})
-     .then(res => {
-         console.log("saaskla");
-         console.log(res);
-         return {
-            type: 'LOAD_SCENARIOS',
-            scenarios: []
-        }
-     }).catch(err => {
-        console.log("in error");
-         console.log(err);
-         return {
-            type: 'LOAD_SCENARIOS',
-            scenarios: []
-         }
-     });
-}
+export const getScenarios = (scenarios) => {
+    return {
+        type: 'GET_SCENARIOS',
+        scenarios
+    };
+};
 
-export const addScenario = ({
-    name = '',
-    isActive = false,
-    interval = 0,
-    nodes = []
-}) => {
+export const startGetScenarios = () => {
+    return (dispatch, getState) => {
+        const username = getState().auth.username;
+        return Axios.get(`/users/${username}/scenarios`)
+        .then(res => {
+            dispatch(getScenarios(res.data.scenarios));
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    };
+};
+
+export const addScenario = (scenario) => {
     return {
         type: 'ADD_SCENARIO',
-        scenario: {
-            id: uuid(),
-            name,
-            isActive,
-            interval,
-            nodes
-        }
+        scenario
+    };
+};
+
+export const startAddScenario = (scenario) => {
+    const body = JSON.stringify(scenario);
+    return (dispatch, getState) => {
+        const username = getState().auth.username;
+        return Axios.post(`/users/${username}/scenarios`, body)
+        .then(res => {
+            dispatch(addScenario(res.data.scenario));
+        })
+        .catch(err => {
+            console.log(err);
+        });
     };
 };
 
@@ -44,12 +46,26 @@ export const removeScenario = (id) => {
         type: 'REMOVE_SCENARIO',
         id
     };
-}
+};
 
 export const editScenario = (id, updates) => {
     return {
         type: 'EDIT_SCENARIO',
         id,
         updates
+    };
+};
+
+export const startEditScenario = (id, updates) => {
+    const body = JSON.stringify(updates);
+    return (dispatch, getState) => {
+        const username = getState().auth.username;
+        return Axios.post(`/users/${username}/scenarios/:scenario_id`, body)
+        .then(res => {
+            dispatch(editScenario(id, res.data.updates));
+        })
+        .catch(err => {
+            console.log(err);
+        });
     };
 };
