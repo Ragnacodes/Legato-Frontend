6a5b326ed012f52634e5e7f78a8f83b740ecb3e7
ali <ali_sedaghi@ymail.com>
2021-04-17 01:02:23 +0430

    [fix] Add forgotten 404 component

---
+++ src/components/Layout/NotFoundPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import PageNotFoundSVG from '../../styles/assets/page-not-found.svg';
+
+const NotFoundPage = () => {
+    return (
+        <div className="centered-container">
+            <img
+                src={PageNotFoundSVG}
+                width="400px"
+                height="400px"
+                alt="404 Page Not Found."
+            />
+            <Button component={NavLink} to="/" color="primary">
+                Back To Home
+            </Button>
+        </div>
+    );
+};
+
+export default NotFoundPage;
