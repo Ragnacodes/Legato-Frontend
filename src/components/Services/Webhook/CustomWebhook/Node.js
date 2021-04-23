bda2b063a8f8da4f53608317620078380d3e81eb
Ysmn <lotfolahi.y@gmail.com>
2021-04-23 16:23:00 +0330

    [fix] node type

--- src/components/Services/Webhook/CustomWebhook/Node.js
+++ src/components/Services/Webhook/CustomWebhook/Node.js
@@ -3,9 +3,7 @@ import Form from './Form';
 import CustomNode from '../../CustomNode';
 
 const Node = (props) => {
-    return (
-        <CustomNode kind="action" form={Form} {...props} />
-    );
+  return <CustomNode kind="trigger" form={Form} {...props} />;
 };
 
 export default Node;
