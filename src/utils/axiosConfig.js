89a0580af8d1515d081b903a4b7d8f6e24cad683
ali <ali_sedaghi@ymail.com>
2021-04-24 03:01:27 +0430

    [fix] Update axios instance when token is set

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -4,6 +4,15 @@ const instance = Axios.create({
     baseURL: 'http://localhost:8080/api'
 });
 
-instance.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+instance.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers.authorization = token;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error),
+  );
 
 export default instance;
