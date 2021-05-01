1736e3d5d3a5457438df79c65ac9da3f62fdf518
Ysmn <lotfolahi.y@gmail.com>
2021-05-01 14:35:36 +0330

    [chore] nodes types and services update

--- src/components/Services/nodeTypes.js
+++ src/components/Services/nodeTypes.js
@@ -1,7 +1,15 @@
-import Webhook1 from './Webhook/CustomWebhook/Node';
+import CustomWebhook from './Webhook/CustomWebhook/Node';
+import WebhookResponse from './Webhook/WebhookResponse/Node';
 import HTTP1 from './HTTP/MakeRequest/Node';
+import AddToPlaylist from './Spotify/SaveToPlaylist/Node';
+import GetTopTracks from './Spotify/GetTopTracks/Node';
+import WatchPlaylist from './Spotify/WatchPlaylist/Node';
 
 export const nodeTypes = {
-    webhooks_: Webhook1,
+    webhooks_customWebhook: CustomWebhook,
+    webhooks_webhookResponse: WebhookResponse,
     https_: HTTP1,
+    spotifies_addToPlaylist: AddToPlaylist,
+    spotifies_getTopTracks: GetTopTracks,
+    spotifies_watchPlaylist: WatchPlaylist,
 };
--- src/components/Services/services.js
+++ src/components/Services/services.js
@@ -1,22 +1,48 @@
 export const services = {
-    webhooks: [
-        {
-            subService: '',
-            kind: 'trigger',
-            primaryText: 'Custom webhook',
-            secondaryText: 'Triggers when webhook receives data.'
-        },
-    ],
-    https: [
-        {
-            subService: '',
-            kind: 'action',
-            primaryText: 'Make a request',
-            secondaryText: 'Sends an HTTP(S) request to a specified URL and processes the response.'
-        },
-    ],
-    spotifys: [],
-    telegrams: [],
-    sshs: [],
-    githubs: [],
+  webhooks: [
+    {
+      subService: 'customWebhook',
+      kind: 'trigger',
+      primaryText: 'Custom webhook',
+      secondaryText: 'Triggers when webhook receives data.',
+    },
+    {
+      subService: 'webhookResponse',
+      kind: 'action',
+      primaryText: 'Webhook response',
+      secondaryText: 'Creates a response to the webhook.',
+    },
+  ],
+  https: [
+    {
+      subService: '',
+      kind: 'action',
+      primaryText: 'Make a request',
+      secondaryText:
+        'Sends an HTTP(S) request to a specified URL and processes the response.',
+    },
+  ],
+  spotifies: [
+    {
+      subService: 'watchPlaylist',
+      kind: 'trigger',
+      primaryText: 'Watch a playlist',
+      secondaryText: 'Triggers when a new track is added to playlist.',
+    },
+    {
+      subService: 'addToPlaylist',
+      kind: 'action',
+      primaryText: 'Add to a playlist',
+      secondaryText: 'Save a track identified by Spotify ID.',
+    },
+    {
+      subService: 'getTopTracks',
+      kind: 'action',
+      primaryText: 'Get top tracks',
+      secondaryText: 'Get user\'s top tracks.',
+    },
+  ],
+  telegrams: [],
+  sshs: [],
+  githubs: [],
 };
