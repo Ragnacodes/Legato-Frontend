039fa28029e0b37d5d90e3cdfae1bc055000399c
ali <ali_sedaghi@ymail.com>
2021-05-13 17:40:40 +0430

    [feat] Get records via action, add loader and no data

--- src/components/ScenarioHistory/ScenarioHistory.js
+++ src/components/ScenarioHistory/ScenarioHistory.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import { connect } from 'react-redux';
+import { startGetHistory } from '../../actions/history';
 import {
     TableContainer,
     Table,
@@ -7,14 +9,32 @@ import {
     TableRow,
     TableCell,
     TableBody,
+    Paper
 } from '@material-ui/core/';
 import Record from './Record';
+import Caption from './Caption';
 
+const ScenarioHistory = ({ records, getHistory }) => {
+    const [loading, setLoading] = useState(true);
+    const scenarioID = useParams().id;
 
-const ScenarioHistory = ({ records }) => {
+    useEffect(() => {
+        setLoading(true);
+        getHistory(scenarioID)
+        .then(() => {
+            setLoading(false);
+        })
+        .catch(() => {
+            setLoading(false)
+        });
+    }, [getHistory, scenarioID]);
+
     return (
-        <TableContainer>
-            <Table>
+        <TableContainer component={Paper}>
+            <Table stickyHeader={true} >
+                { loading && <Caption caption="Loading ..." />}
+                { !loading && !records.length && <Caption caption="No Data." />}
+                
                 <TableHead>
                     <TableRow>
                         <TableCell align="left">Started</TableCell>
@@ -22,13 +42,14 @@ const ScenarioHistory = ({ records }) => {
                         <TableCell align="left">Duration</TableCell>
                         <TableCell align="left">Operation</TableCell>
                         <TableCell align="left">Data Transfer</TableCell>
+                        <TableCell align="right"></TableCell>
                     </TableRow>
                 </TableHead>
 
                 <TableBody>
                     {
                         records.map((record, index) => {
-                            return <Record key={index} record={record} />
+                            return <Record key={index} {...record} />
                         })
                     }
                 </TableBody>
@@ -43,4 +64,10 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default connect(mapStateToProps)(ScenarioHistory);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getHistory: (scenarioID) => dispatch(startGetHistory(scenarioID))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ScenarioHistory);
