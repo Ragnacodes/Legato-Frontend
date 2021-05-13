06fc374fd002e3071bc781b16990dedcb60947aa
ali <ali_sedaghi@ymail.com>
2021-05-13 17:35:01 +0430

    [feat] Mock async get history

--- src/actions/history.js
+++ src/actions/history.js
@@ -1,6 +1,24 @@
+import { defaultHistoryState } from '../playground/history';
+
 export const getHistory = (history) => {
     return {
-        type: '',
+        type: 'GET_HISTORY',
         history
+    };
+};
+
+export const startGetHistory = (scenarioID) => {
+    return (dispatch, getState) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(defaultHistoryState);
+            }, 3000);
+        })
+        .then(res => {
+            dispatch(getHistory(res));
+        })
+        .catch(err => {
+            console.log(err);
+        });
     };
 };
