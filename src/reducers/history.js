c561118c33521622e8a249e24e402822be825f36
ali <ali_sedaghi@ymail.com>
2021-05-13 17:35:45 +0430

    [fix] Remove hardcoded history state

--- src/reducers/history.js
+++ src/reducers/history.js
@@ -1,23 +1,4 @@
-const defaultHistoryState = [
-    {
-        id: 1,
-        time: '5 april 2021 22:12',
-        status: 'success',
-        duration: 2,
-        operation: 3,
-        data: 6
-    },
-    {
-        id: 2,
-        time: '7 may 2021 07:16',
-        status: 'failure',
-        duration: 1,
-        operation: 1,
-        data: 3
-    },
-]
-
-const historyReducer = (state=defaultHistoryState, action) => {
+const historyReducer = (state=[], action) => {
     switch (action.type) {
         case 'GET_HISTORY':
             return action.history;
