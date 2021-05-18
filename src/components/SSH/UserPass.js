f6c56a4664feee9d0a6311acc9241dbf0b4aacb7
Ysmn <lotfolahi.y@gmail.com>
2021-05-18 21:23:01 +0330

    [feat] show password icon

--- src/components/SSH/UserPass.js
+++ src/components/SSH/UserPass.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { TextField } from '@material-ui/core';
+import React,{ useState } from 'react';
+import { TextField, InputAdornment, IconButton } from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
 
 const UserPass = ({ info, handleChange }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="password">
       <TextField
-        type="password"
+        type={showPassword ? 'text' : 'password'}
         name="password"
         className="text-field"
         label="Password"
@@ -13,6 +16,20 @@ const UserPass = ({ info, handleChange }) => {
         size="small"
         value={info['password']}
         onChange={(e) => handleChange(e.target.name, e.target.value)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword((prev) => !prev)}
+                onMouseDown={(e) => e.preventDefault()}
+                edge="end"
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
       />
     </div>
   );
