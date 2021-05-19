b1c90b887715ae6237870900225ec06f435b5688
ali <ali_sedaghi@ymail.com>
2021-05-19 21:01:44 +0430

    [style] Update webhook icon

--- src/components/Services/Webhook/CustomServiceNode.js
+++ src/components/Services/Webhook/CustomServiceNode.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import CustomNode from '../CustomNode';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import Webhook from '../../../styles/assets/webhook.svg';
 
 const CustomServiceNode = (props) => {
     const shape = (
         <div className="node webhook">
-            <FontAwesomeIcon icon={faGithub} className="webhook" />
+            <img src={Webhook} alt="webhook-logo" width="75" className="webhook" />
         </div>
     );
 
--- src/components/Services/Webhook/SidebarItem.js
+++ src/components/Services/Webhook/SidebarItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import CustomSidebarItem from '../CustomSidebarItem';
+import Webhook from '../../../styles/assets/webhook.svg';
 
 const SidebarItem = () => {
     return (
         <CustomSidebarItem
-            icon={<FontAwesomeIcon icon={faGithub} className="icon webhook" />}
+            icon={<img src={Webhook} alt="webhook-logo" width="35" className="icon webhook" />}
             serviceName="webhooks"
         />
     );
