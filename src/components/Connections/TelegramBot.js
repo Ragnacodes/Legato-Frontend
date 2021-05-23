15bc441bd2927796b492604f2647cfbed105ff53
arman <armanalaki@gmail.com>
2021-05-23 22:26:49 +0430

    [feat] initial telegram bot connection

---
+++ src/components/Connections/TelegramBot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Dialog } from '@material-ui/core';
+
+const TelegramBot = ({telegramDialog, setTelegramDialog}) => {
+    return (
+        <Dialog>
+            
+        </Dialog>
+    )
+}
+
+export default TelegramBot;
