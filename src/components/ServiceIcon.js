9e52b18a2158a2c68bf0429d6aab3df75ee30ae2
Ysmn <lotfolahi.y@gmail.com>
2021-05-26 20:41:08 +0330

    [feat] service icon component

---
+++ src/components/ServiceIcon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import clsx from 'clsx';
+
+const Icon = ({service, size, className}) => {
+    const res = size==='small'? 64 : 256;
+    const imageName = `${service}_${res}`;
+    const getImage = (image) => {
+        return require('../styles/assets/services/' + image + '.png').default
+    }
+    return (
+        <div className={clsx(className, `icon-component service-icon--${service}`)}>
+        <img src={getImage(imageName)} alt={`${service}-logo`} />
+        </div>  
+    )
+};
+
+export default Icon;
