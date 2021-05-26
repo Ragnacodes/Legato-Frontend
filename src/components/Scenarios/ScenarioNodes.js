c87678671d46020d785f2b1afbc62d6035173a51
Ysmn <lotfolahi.y@gmail.com>
2021-05-26 21:20:01 +0330

    [chore] replaced scenario avatars

--- src/components/Scenarios/ScenarioNodes.js
+++ src/components/Scenarios/ScenarioNodes.js
@@ -1,55 +1,53 @@
 import React from 'react';
 import { AvatarGroup } from '@material-ui/lab';
 import { Avatar } from '@material-ui/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDiscord, faSpotify, faTelegram, faGithub } from '@fortawesome/free-brands-svg-icons';
-
+import ServiceIcon from '../ServiceIcon';
 const ScenarioNodes = ({nodes}) => {
     return (
-        <AvatarGroup max={4}>
+        <AvatarGroup max={4} spacing={5}>
             {
                 nodes.reverse().map((node, index) => {
                     switch (node) {
                         case 'sshes':
                             return (
-                                <Avatar key={index} className="ssh">
-                                    <FontAwesomeIcon icon={faGithub} />
+                                <Avatar key={index} className="scenario-avatar ssh">
+                                    <ServiceIcon service='ssh' size='small' className='scenario-icons'/>
                                 </Avatar>
                             );
                         case 'webhooks':
                             return (
-                                <Avatar key={index} className="webhook">
-                                    <FontAwesomeIcon icon={faGithub} />
+                                <Avatar key={index} className="scenario-avatar webhook">
+                                    <ServiceIcon service='webhooks' size='small' className='scenario-icons'/>
                                 </Avatar>
                             );
                         case 'githubs':
                             return (
-                                <Avatar key={index} className="github">
-                                    <FontAwesomeIcon icon={faGithub} />
+                                <Avatar key={index} className="scenario-avatar github">
+                                    <ServiceIcon service='github' size='small' className='scenario-icons'/>
                             </Avatar>
                             );
                         case 'spotifies':
                             return (
-                                <Avatar key={index} className="spotify">
-                                    <FontAwesomeIcon icon={faSpotify} />
+                                <Avatar key={index} className="scenario-avatar spotify">
+                                    <ServiceIcon service='spotify' size='small' className='scenario-icons'/>
                                 </Avatar>
                             );
                         case 'telegrams':
                             return (
-                                <Avatar key={index} className="telegram">
-                                    <FontAwesomeIcon icon={faTelegram} />
+                                <Avatar key={index} className="scenario-avatar telegram">
+                                    <ServiceIcon service='telegram' size='small' className='scenario-icons'/>
                                 </Avatar>
                             );
                         case 'https':
                             return (
-                                <Avatar key={index} className="http">
-                                    <FontAwesomeIcon icon={faGithub} />
+                                <Avatar key={index} className="scenario-avatar http">
+                                    <ServiceIcon service='http' size='small' className='scenario-icons'/>
                                 </Avatar>
                             );
                         case 'discord':
                             return (
-                                <Avatar key={index} className="discord">
-                                    <FontAwesomeIcon icon={faDiscord} />
+                                <Avatar key={index} className="scenario-avatar discord">
+                                    <ServiceIcon service='discord' size='small' className='scenario-icons'/>
                                 </Avatar>
                             );
                         default:
