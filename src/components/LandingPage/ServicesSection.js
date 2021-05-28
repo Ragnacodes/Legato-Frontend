804d9d7e796f6c240f67a5ca084218d690edc619
ali <ali_sedaghi@ymail.com>
2021-05-28 07:36:37 +0430

    [refactor] Landing page service icons use an abstarct component

--- src/components/LandingPage/ServicesSection.js
+++ src/components/LandingPage/ServicesSection.js
@@ -1,36 +1,11 @@
 import React from 'react';
 import { Container, Grid, Typography, Box } from '@material-ui/core';
-import discordIcon from '../../styles/assets/services/discord_256.png';
-import githubIcon from '../../styles/assets/services/github_256.png';
-import gmailIcon from '../../styles/assets/services/gmail_256.png';
-import httpIcon from '../../styles/assets/services/http_256.png';
-import regexIcon from '../../styles/assets/services/regex_256.png';
-import repeaterIcon from '../../styles/assets/services/repeater_256.png';
-import sleepIcon from '../../styles/assets/services/sleep_256.png';
-import spotifyIcon from '../../styles/assets/services/spotify_256.png';
-import sshIcon from '../../styles/assets/services/ssh_256.png';
-import telegramIcon from '../../styles/assets/services/telegram_256.png';
-import webhooksIcon from '../../styles/assets/services/webhooks_256.png';
+import LandingServiceIcon from '../ServiceIcons/LandingServiceIcon';
 
 const Service = ({ name, src }) => {
     return (
         <Grid item lg={1} sm={2} xs={3}>
-            <Box
-                p={2}
-                className={`
-                    section__service
-                    service-icon
-                    service-icon--${name.toLowerCase()}
-                `}
-            >
-                <Grid container justify="center">
-                    <img
-                        src={src}
-                        alt={name}
-                        width="100%"
-                    />
-                </Grid>
-            </Box>
+            <LandingServiceIcon service={name.toLowerCase()} />
             <Grid container justify="center">
                 <Typography variant="caption" align="center">{name}</Typography>
             </Grid>
@@ -67,17 +42,17 @@ const ServicesSection = () => {
                 </Typography>
                 <Box mt={4}>
                     <Grid container spacing={2} justify="center">
-                        <Service name="Webhooks" src={webhooksIcon} />
-                        <Service name="HTTP" src={httpIcon} />
-                        <Service name="SSH" src={sshIcon} />
-                        <Service name="GitHub" src={githubIcon} />
-                        <Service name="Gmail" src={gmailIcon} />
-                        <Service name="Telegram" src={telegramIcon} />
-                        <Service name="Spotify" src={spotifyIcon} />
-                        <Service name="Discord" src={discordIcon} />
-                        <Service name="Repeater" src={repeaterIcon} />
-                        <Service name="Sleep" src={sleepIcon} />
-                        <Service name="Regex" src={regexIcon} />
+                        <Service name="Webhooks" />
+                        <Service name="HTTP" />
+                        <Service name="SSH" />
+                        <Service name="GitHub" />
+                        <Service name="Gmail" />
+                        <Service name="Telegram" />
+                        <Service name="Spotify" />
+                        <Service name="Discord" />
+                        <Service name="Repeater" />
+                        <Service name="Sleep" />
+                        <Service name="Regex" />
                     </Grid>
                 </Box>
             </Container>
