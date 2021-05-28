16042c4c7b70dece887e7b49ad09669736f1e803
ali <ali_sedaghi@ymail.com>
2021-05-28 06:48:17 +0430

    [refactor] Service icon abstract file is styled with material

--- src/components/ServiceIcon.js
+++ src/components/ServiceIcon.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import clsx from 'clsx';
+import { Box, Grid } from '@material-ui/core';
 
-const Icon = ({service, size, className}) => {
-    const res = size==='small'? 64 : 256;
+const getImage = (image) => {
+    return require('../styles/assets/services/' + image + '.png').default;
+};
+
+const ServiceIcon = ({ service, size, padding, className }) => {
+    const res = size === 'small' ? 64 : 256;
     const imageName = `${service}_${res}`;
-    const getImage = (image) => {
-        return require('../styles/assets/services/' + image + '.png').default
-    }
+
     return (
-        <div className={clsx(className, `icon-component service-icon--${service}`)}>
-        <img src={getImage(imageName)} alt={`${service}-logo`} draggable={false} />
-        </div>  
+        <Box
+            p={padding}
+            className={
+                clsx(
+                    'service-icon',
+                    `service-icon--${service}`,
+                    className
+                )
+            }
+        >
+            <Grid
+                container
+                justify="center"
+            >
+                <img
+                    src={getImage(imageName)}
+                    alt={`${service}-logo`}
+                    draggable={false}
+                    width="100%"
+                />
+            </Grid>
+        </Box>
     )
 };
 
-export default Icon;
+export default ServiceIcon;
