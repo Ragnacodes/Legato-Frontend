e1e2a6625b3288ac1b25bd5774c4928c6fed417d
ali <ali_sedaghi@ymail.com>
2021-05-31 07:21:54 +0430

    [feat] Print log messages of SSE

--- src/components/History/CustomAccordion.js
+++ src/components/History/CustomAccordion.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { serviceBackToFront } from '../../utils/serviceConverter';
 import {
+    Box,
     Grid,
     Accordion,
     AccordionSummary,
@@ -9,7 +11,7 @@ import {
 import { ExpandMore } from '@material-ui/icons';
 import Status from './Status';
 
-const CustomAccordion = ({ id, service, messages }) => {
+const CustomAccordion = ({ status, service, messages }) => {
     return (
         <Accordion>
             <AccordionSummary
@@ -17,26 +19,45 @@ const CustomAccordion = ({ id, service, messages }) => {
             >
                 <Grid
                     container
+                    alignItems="center"
                     spacing={2}
                 >
                     <Grid item>
-                        <Status status={"hi"} />
+                        <Grid container justify="center">
+                            <Status status={status} />
+                        </Grid>
                     </Grid>
                     <Grid item>
-                        <Typography>{service.type}</Typography>
+                        <Typography>{serviceBackToFront(service.type)}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography>|</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="body2">{service.subType}</Typography>
                     </Grid>
                 </Grid>
             </AccordionSummary>
             <AccordionDetails>
-                {
-                    messages.map((message, index) => {
-                        return (
-                            <Typography key={index}>
-                                {message.context}
-                            </Typography>
-                        );
-                    })
-                }
+                <Box px={4}>
+                    <Grid
+                        container
+                        spacing={1}
+                        direction="column"
+                    >
+                        {
+                            messages.map((message, index) => {
+                                return (
+                                    <Grid item key={index}>
+                                        {/* <Typography> */}
+                                            {message.context}
+                                        {/* </Typography> */}
+                                    </Grid>
+                                );
+                            })
+                        }
+                    </Grid>
+                </Box>
             </AccordionDetails>
         </Accordion>
     );
