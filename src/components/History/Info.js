c66ad371a2ab2719db1102be967ca8fc0e0b79f9
ali <ali_sedaghi@ymail.com>
2021-05-31 07:20:23 +0430

    [feat] Add new field to info section of logs

--- src/components/History/Info.js
+++ src/components/History/Info.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { Typography, Paper, Divider, Container } from '@material-ui/core';
+import { Paper, Container } from '@material-ui/core';
 import LabelValue from './LabelValue';
 
+const statusToText = (status) => {
+    switch (status) {
+        case 1: return 'Success';
+        case 0: return 'Pending';
+        case -1: return 'Failed';
+        default: return 'Unknwon';
+    };
+};
+
 const Info = ({ history }) => {
-    const { time, status, duration, operation, data } = history;
+    const { date, time, status, duration, operations, data } = history;
     return (
         <Container
             component={Paper}
             className="full-height padding"
         >
-            <Typography align="center" className="margin-v">{time}</Typography>
-            <Divider className="margin-v2"/>
-            <LabelValue label="Status" value={status} />
+            <LabelValue label="Date" value={date} />
+            <LabelValue label="Time" value={time} />
+            <LabelValue label="Status" value={statusToText(status)} />
             <LabelValue label="Duration" value={duration} />
-            <LabelValue label="Operation" value={operation} />
+            <LabelValue label="Operation" value={operations} />
             <LabelValue label="Data" value={data} />
         </Container>
     );
