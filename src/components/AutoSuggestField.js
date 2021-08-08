ac3ff9899f9c0eda8682a93f2a5d12b12f3ae381
Ali Sedaghi <ali_sedaghi@ymail.com>
2021-08-08 06:42:36 +0430

    [feat] Add auto suggest component suggestting only nodes

---
+++ src/components/AutoSuggestField.js
@@ -0,0 +1,55 @@
+import React, { forwardRef, useState } from 'react';
+import { TextField } from '@material-ui/core';
+import TextInput from 'react-autocomplete-input';
+
+const findLastPart = (text) => {
+    const lastDotIndex = text.lastIndexOf('.');
+    const lastDollarIndex = text.lastIndexOf('$');
+    const greaterIndex = lastDotIndex > lastDollarIndex ? lastDotIndex : lastDollarIndex;
+    const lastTrigger = text.charAt(greaterIndex);
+    const lastSlug = text.substring(greaterIndex + 1);
+    return {
+        lastTrigger,
+        lastSlug
+    };
+};
+
+const CustomTextarea = forwardRef((props, ref) => {
+    return (
+        <TextField
+            inputRef={ref}
+            {...props}
+        />
+    );
+});
+
+const AutoSuggestField = (props) => {
+    const {ancestors, ...other} = props;
+    const parents = ancestors.map(item => item.id.toString());
+
+    const [options] = useState({
+        '$': parents,
+        '.': []
+    });
+
+    // returns the whole text
+    const handleSelect = (text) => {
+        const {lastTrigger, lastSlug} = findLastPart(text);
+        console.log(lastTrigger);
+        console.log(lastSlug);
+
+    };
+
+    return (
+        <TextInput
+            trigger={["$", "."]}
+            spacer=""
+            options={options}
+            onSelect={handleSelect}
+            Component={CustomTextarea}
+            {...other}
+        />
+    );
+};
+
+export default AutoSuggestField;
